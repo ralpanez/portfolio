8c6345fab7a77abeb03010002ee62fd285414cf5
Rafael A. <rafabcn1991@gmail.com>
2025-06-02 21:23:13 +0200

    Actualizo el portfolio añadiendo JS

---
+++ main.js
@@ -0,0 +1,4 @@
+// Mensaje simpático en la consola
+console.log('%cGracias por revisar mi portfolio!', 'color: #0d6efd; font-size: 16px; font-weight: bold;');
+
+console.log('%cDesarrollado con HTML, CSS y JS!', 'color: #084298; font-size: 13px;');
